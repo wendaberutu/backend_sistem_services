0a251cfe0edd3e84d221f025ce19cee83ba2f90e
wenda <wendaberutu22@gmail.com>
2026-02-05 16:30:39 +0700

    1. membuat backend listen ke ip manapun

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,9 +2,14 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   const token = req.cookies?.access_token;
+
   if (!token) {
-    return res.status(401).json({ success: false, message: "No token provided" });
+    return res.status(401).json({
+      success: false,
+      message: "No token provided",
+    });
   }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = {
@@ -13,6 +18,10 @@ module.exports = (req, res, next) => {
     };
     next();
   } catch (e) {
-    return res.status(401).json({ success: false, message: "Invalid token" });
+    return res.status(401).json({
+      success: false,
+      message: "Invalid token",
+    });
   }
 };
+
